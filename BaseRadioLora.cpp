c32fa2fe33bb0a7447773b6444fff02380a000d9
David C <disco47dave@gmail.com>
2019-11-03 23:51:27 -0600

    Trying stuff

--- BaseRadioLora.cpp
+++ BaseRadioLora.cpp
@@ -29,7 +29,7 @@ BaseRadioLora  --  runs on Arduino Nano and acts as a serial to LoRa bridge
 #define DEBUG(x)
 #endif
 
-#define HOLDING_BUFFER_SIZE 248
+#define HOLDING_BUFFER_SIZE 20
 
 #define RFM95_CS 10
 #define RFM95_RST 9
@@ -120,16 +120,6 @@ void listenToRadio() {
 			processRadioBuffer(buf);
 		}
 	}
-
-//	static boolean didIt = false;
-//	if(!didIt){
-//		didIt = true;
-//		Serial.print("<");
-//		Serial.print(freeRam());
-//		Serial.print(">");
-//
-//	}
-
 }
 
 //TODO:
@@ -237,11 +227,12 @@ void flush(){
 
 void handleSerialRaw(char* p) {
 	uint8_t len = p[2];
-	addToHolding((uint8_t*) p, len);
-	if(flushOnNextRaw){
-		flushOnNextRaw = false;
-		flush();
-	}
+//	addToHolding((uint8_t*) p, len);
+//	if(flushOnNextRaw){
+//		flushOnNextRaw = false;
+//		flush();
+//	}
+	sendToRadio((uint8_t*)p, len);
 }
 
 void handleSerial(char *p) {
@@ -253,6 +244,7 @@ void handleSerial(char *p) {
 	if (strcmp(p, "<FFE>") == 0) {
 		flush();
 	} else {
-		addToHolding(p);
+//		addToHolding(p);
+		sendToRadio(p);
 	}
 }
